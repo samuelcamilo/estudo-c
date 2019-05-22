c8c4083e8d4932abd6be7b7cc2095b801a04d16a
samuelcamilo <ssamuelcamilo@gmail.com>
2019-05-22 01:53:24 -0300

    metodo: quantidade de pacientes

--- static-list/main.c
+++ static-list/main.c
@@ -1,146 +1,89 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <locale.h>
-#define TAM 2
+#define TAM 5
 
-typedef struct {
+typedef struct 
+{
+    int codigo;
     char nome[50];
     int idade;
     float altura;
     float peso;
-    float imc;
-}Pessoa;
+} Cliente;
 
-Pessoa pessoa;
+typedef struct 
+{
+    int qtd;
+    Cliente cliente[TAM];
+}Lista;
 
 void menu();
-void inicializar();
-void cadastraPessoa();
-void calculaIMC();
-void exibeLista();
+Lista *inicializaLista();
+void liberaLista();
 
 int main()
 {
-    setlocale(LC_ALL, "portuguese");
+    //Lista *lista;
+    //lista = inicializaLista();
+    //printf("%d\n", lista->qtd);
+    //liberaLista(lista);
     menu();
-    return 0;
 }
 
 void menu()
-{
+{   
+    Lista *lista;
     int opcao;
-    Pessoa pessoa[TAM];
-
     do
     {
-        printf("===== Menu =====\n");
-        printf("1 - Inicializar\n");
-        printf("2 - Cadastrar Pessoa\n");
-        printf("3 - Calcular Imc\n");
-        printf("4 - Exibir Lista\n");
-        printf("0 - Sair\n");
-        printf("==================\n");
-        printf("Digite uma opção: ");
+        printf("----- MENU -----\n");
+        printf("(1) Cadastrar Paciente\n");
+        printf("(2) Buscar Paciente\n");
+        printf("(3) Ordenar Por IMC\n");
+        printf("(4) Tamanho Lista\n");
+        printf("(0) Sair\n");
+        printf("----------------\n");
+        printf("Digite uma Opção: ");
         scanf("%d", &opcao);
+
+        lista = inicializaLista();
 
         switch(opcao)
         {
             case 1:
-                system("clear");
-                printf("Lista Inicializada!...\n");
-                inicializar(pessoa);
+                //lista = inicializaLista();
+                //printf("Tamanho: %d\n", lista->qtd);
                 break;
             case 2:
-                system("clear");
-                printf("===== Cadastro =====\n");
-                cadastraPessoa(pessoa);
-                system("clear");
                 break;
             case 3:
-                system("clear");
-                printf("===== Calculo IMC =====\n");
-                calculaIMC(pessoa);
                 break;
             case 4:
                 system("clear");
-                printf("===== Exibindo =====\n");
-                exibeLista(pessoa);
-                printf("====================\n");
-                break;
-            case 0:
-                system("clear");
-                printf("Saindo!\n");
+                printf("----- Tamanho Lista -----\n");
+                printf("Tamanho lista de pacientes: %d\n", lista->qtd);
+                printf("-------------------------\n");
                 break;
             default:
-                system("clear");
-                printf("Opção Inválida!\n");
                 break;
         }
 
-    } while (opcao != 0);   
-}
-
-void inicializar(Pessoa lista[])
-{
-    int cont;
-
-    for(cont = 0; cont < TAM; cont++)
-    {
-        strcpy(lista[cont].nome, "NULL");
-        lista[cont].idade = 0;
-        lista[cont].altura = 0.0;
-        lista[cont].peso = 0.0;
-        lista[cont].imc = 0.0;    
-    }
+    } while (opcao != 0);
     
 }
 
-void cadastraPessoa(Pessoa lista[])
+Lista *inicializaLista()
 {
-    int cont;
+    Lista *li;
+    li = (Lista*) malloc(sizeof(Lista));
 
-    for(cont = 0; cont < TAM; cont++)
-    {
-        printf("Digite Seu Nome: ");
-        scanf("%s", lista[cont].nome);
-        getchar();
+    if(li != NULL)
+        li->qtd = 0;
 
-        printf("Digite Sua Idade: ");
-        scanf("%d", &lista[cont].idade);
-
-        printf("Digite Sua Altura: ");
-        scanf("%f", &lista[cont].altura);
-
-        printf("Digite Seu Peso: ");
-        scanf("%f", &lista[cont].peso);
-    }
+    return li;
 }
 
-void calculaIMC(Pessoa lista[])
+void liberaLista(Lista *li)
 {
-    int cont;
-
-    for(cont = 0; cont < TAM; cont++)
-    {
-        lista[cont].imc = (lista[cont].peso / (lista[cont].altura * lista[cont].altura));
-        printf("IMC do %s Calculado com Sucesso!\n", lista[cont].nome);
-    }
-    
-}
-
-void exibeLista(Pessoa lista[])
-{
-    int cont;
-
-    for(cont = 0; cont < TAM; cont++)
-    {
-        printf("Nome: %s\n", lista[cont].nome);
-        printf("Idade: %d\n", lista[cont].idade);
-        printf("Altura: %.2f\n", lista[cont].altura);
-        printf("Peso: %.2f\n", lista[cont].peso);
-        printf("IMC: %.2f\n", lista[cont].imc);
-        printf("====================\n");
-    }
-    
+    free(li);
 }
